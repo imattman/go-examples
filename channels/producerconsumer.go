1cdd8136b34b6271ddcfc1ad3d51afad00e7372c
Matt Phillips <mattman@mattcave.com>
2017-02-19 09:50:28 -0800

    added variation of prod/consume timing

--- channels/producerconsumer.go
+++ channels/producerconsumer.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -17,7 +18,10 @@ type WorkUnit struct {
 }
 
 func (w WorkUnit) String() string {
-	return fmt.Sprintf("p(%d-%d) c(%d-%d)", w.producer, w.pseq, w.consumer, w.cseq)
+	return fmt.Sprintf("p(%d-%d) c(%d-%d) produce: %12s  consume: %12s",
+		w.producer, w.pseq,
+		w.consumer, w.cseq,
+		w.prodTime, w.consTime)
 }
 
 func main() {
@@ -28,12 +32,15 @@ func main() {
 	var cAvg time.Duration
 	var cVar time.Duration
 
+	var maxQuiet time.Duration
+
 	flag.IntVar(&pcnt, "p", 1, "producer count")
 	flag.DurationVar(&pAvg, "pavg", (100 * time.Millisecond), "average produce duration")
-	flag.DurationVar(&pAvg, "pvar", (50 * time.Millisecond), "produce duration variance")
+	flag.DurationVar(&pVar, "pvar", (50 * time.Millisecond), "produce duration variance")
 	flag.IntVar(&ccnt, "c", 1, "consumer count")
 	flag.DurationVar(&cAvg, "cavg", (100 * time.Millisecond), "average consume duration")
-	flag.DurationVar(&cAvg, "cvar", (50 * time.Millisecond), "consume duration variance")
+	flag.DurationVar(&cVar, "cvar", (50 * time.Millisecond), "consume duration variance")
+	flag.DurationVar(&maxQuiet, "max", (2 * time.Second), "max quiet threshold of no data")
 	flag.Parse()
 
 	prodcons := make(chan WorkUnit)
@@ -48,12 +55,11 @@ func main() {
 		go produce(ctx, i, prodcons, pAvg, pVar)
 	}
 
-	maxQuiet := 2 * time.Second
 	for i := 0; i < 20; i++ {
 		select {
 		case work := <-finished:
 			fmt.Printf("complete: %s\n", work)
-		case <-time.After(1 * time.Second):
+		case <-time.After(maxQuiet):
 			fmt.Printf("Exceeded %s max quiet period of no data\n", maxQuiet)
 			cancelFunc()
 			return
@@ -64,8 +70,7 @@ func main() {
 
 func produce(ctx context.Context, id int, data chan<- WorkUnit, pAvg, pVar time.Duration) {
 	for i := 0; true; i++ {
-		prodTime := pAvg
-
+		prodTime := duration(pAvg, pVar)
 		time.Sleep(prodTime)
 		w := WorkUnit{producer: id, pseq: i, prodTime: prodTime}
 		select {
@@ -79,7 +84,7 @@ func produce(ctx context.Context, id int, data chan<- WorkUnit, pAvg, pVar time.
 
 func consume(ctx context.Context, id int, data <-chan WorkUnit, collect chan<- WorkUnit, cAvg, cVar time.Duration) {
 	for i := 0; true; i++ {
-		consTime := cAvg
+		consTime := duration(cAvg, cVar)
 		select {
 		case w := <-data:
 			w.consumer = id
@@ -92,4 +97,15 @@ func consume(ctx context.Context, id int, data <-chan WorkUnit, collect chan<- W
 			return
 		}
 	}
+}
+
+func duration(avg, variance time.Duration) time.Duration {
+	offset := avg.Nanoseconds() - variance.Nanoseconds()
+	ubound := 2 * variance.Nanoseconds()
+	if ubound <= 0 {
+		ubound = 1
+	}
+	nsecs := rand.Int63n(ubound) + offset
+
+	return time.Duration(nsecs)
 }
